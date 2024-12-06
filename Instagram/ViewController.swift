8284fc570764ed0df6d39a3b7056f268b6f465a0
smh <soeminhtut73@gmail.com>
2024-12-06 16:53:38 +0800

    add origin url

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by S M H  on 06/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
